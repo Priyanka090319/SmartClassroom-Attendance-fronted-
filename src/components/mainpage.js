a1bcb1969b8f13f955176a9ae11c9292d15edcd5
Priyanka singh <as5207483@gmail.com>
2025-06-26 15:04:11 +0530

    Update mainpage.js

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -7,37 +7,7 @@ const ClassroomApp = () => {
 
   return (
     
-    <div className="mainpage-wrapper">import React, { useState } from 'react';
-import LoginPage from './login_signup';
-import '../styles/mainpage.css';
-
-const ClassroomApp = () => {
-  const [started, setStarted] = useState(false);
-
-  return (
-    
     <div className="mainpage-wrapper">
-      {started === false ? (
-
-        <div className="box">
-
-          <div className="left-content">
-            <h1>Welcome to Smart Classroom</h1>
-            <button onClick={() => setStarted(true)}>Get Started</button>
-          </div>
-
-          <div className="right-content">
-            <img src="/classroom.jpg" alt="classroom" />
-          </div>
-
-        </div>
-      ) : ( <LoginPage />  )}
-
-    </div>
-  );
-};
-
-export default ClassroomApp;
       {started === false ? (
 
         <div className="box">
